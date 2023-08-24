0a06fa362f8383083e674b10e2d53dd3e1e1aef5
booyaChael <chaeeul2022@gmail.com>
2023-08-24 22:09:42 +0900

    사칙연산 1001 해결

--- src/Basic1/quiz_1001.java
+++ src/Basic1/quiz_1001.java
@@ -1,10 +1,12 @@
+package Basic1;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
+public class quiz_1001 {
+    public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s=br.readLine().split(" ");
