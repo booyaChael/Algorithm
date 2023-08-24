8371a0e37e68709da928c6fb447411fddbb87889
booyaChael <chaeeul2022@gmail.com>
2023-08-24 21:59:56 +0900

    2557, 1000 풀이

---
+++ src/Basic1/quiz_1000.java
@@ -0,0 +1,17 @@
+package Basic1;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class quiz_1000 {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s=br.readLine().split(" ");
+
+        System.out.println(Integer.parseInt(s[0])+Integer.parseInt(s[1]));
+
+    }
+}
